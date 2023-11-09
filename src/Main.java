2f46785edda16ba69d1ba87fb2125bf2c2519db0
BT_4N2_33 <jasont156@nycstudents.net>
2023-11-09 14:04:02 -0500

    seventh commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("You've been kidnapped and forced to choose a car\nThere is no information other than you'll face consequences if you don't choose\nYou have four choices:\n(1 for car 1, 2 for car 2, 3 for car 3, 4 for car 4)\nWhich car will you pick?");
+        System.out.println("\nYou've been kidnapped and forced to choose a car\nThere is no information other than you'll face consequences if you don't choose\nYou have four choices:\n(1 for car 1, 2 for car 2, 3 for car 3, 4 for car 4)\nWhich car will you pick?");
         String car = s.nextLine();
-        Introduction player = new Introduction(0, 0, 10, 0);
+        Race player = new Race(0, 0, 10, 0);
         int carChoice = 0;
         try {
             carChoice = Integer.parseInt(car);
@@ -19,14 +19,15 @@ public class Main {
                 System.out.println(player.car4());
             } else {
                 System.out.println("That is not a car choice\nBecause you cannot pick between 4 numbers, a car will be given to you now randomly\n");
-                System.out.println(player.unluckyThen());
+                System.out.println(player.randomCar());
             }
         } catch (NumberFormatException e) {
             System.out.println("You can't escape it\nSince you don't know what a number is, a car will be given to you now randomly\n");
-            System.out.println(player.unluckyThen());
+            System.out.println(player.randomCar());
         }
 
         car = s.nextLine();
+
         if (car.equals("y")) {
             System.out.println("The rules will be presented to you after this line\n");
             System.out.println(player.beginGame());
@@ -34,7 +35,7 @@ public class Main {
             System.out.println("You cannot flee from this event LOL try harder next time\nYour instructions will be presented to you now\n");
             System.out.println(player.beginGame());
         }
-            System.out.print("What is the upper and lower limit of the integer numbers you want to be working with?\nIf you don't pick a number, the limits will be set to ten and zero, respectively\nUpper: ");
+            System.out.print("What is the upper and lower limit of the integer numbers you want to be working with?\nUpper: ");
 
         try {
             int upperLimit = Integer.parseInt(s.nextLine());
@@ -43,11 +44,12 @@ public class Main {
             int lowerLimit = Integer.parseInt(s.nextLine());
             player.getLower(lowerLimit);
         } catch (NumberFormatException e) {
-            System.out.println("You're no fun huh? Limits will be 10 and 0 now.");
+            System.out.println("You're no fun huh? Your limits will be set to 10 and 0 now.");
         }
 
         System.out.println("We will begin after the three second countdown");
         player.countdown();
+
 
         System.out.print(player.equation());
         int answer = Integer.parseInt(s.nextLine());
@@ -64,7 +66,7 @@ public class Main {
             }
         }
 
-        System.out.println(player.finalStats());
+        System.out.println(player.toString());
 
     }
 }
--- src/Race.java
+++ src/Race.java
@@ -1,3 +1,186 @@
-public class Race {
+import java.util.concurrent.TimeUnit;
 
+public class Race {
+    private int score, lower, upper, incorrect;
+    private int random1, random2;
+    private String sign;
+
+    /**
+     * Race represents the numbers used to calculate a problem
+     */
+    public Race(int answer, int lower, int upper, int incorrect) {
+        this.lower = lower;
+        this.upper = upper;
+        this.score = answer;
+        this.incorrect = incorrect;
+    }
+
+    /**
+     *car1 - car4 represents descriptions of cars
+     */
+    public String car1() {
+        return ("You're given a blue Mclaren\nThis supercar has a devastating 0 horses\nYou need to help it win the race coming up\nAre you ready? (y for yes // n for no)");
+    }
+
+    public String car2() {
+        return ("You're given a golf cart\nThis small little vehicle has a whopping 700 horsepower\nYou need to help it win the race coming up\nAre you ready? (y for yes // n for no)");
+    }
+
+    public String car3() {
+        return "You're given a Jeep truck\nThis monster of a machine has a decent 100 horses\nYou need to help it win the race coming up\nAre you ready? (y for yes // n for no)";
+    }
+
+    public String car4() {
+        return ("You're given a Tesla (let's go Elon Musk)\nThis electric powered vehicle has a whopping 483 horsepower???\nYou need to help it win the race coming up\nAre you ready? (y for yes // n for no)");
+    }
+
+    /**
+     * randomCar returns a random car description
+     */
+
+    public String randomCar() {
+        int randomCar = (int) (Math.random() * 4) + 1;
+        if (randomCar == 4) {
+            return car4();
+        } else if (randomCar == 3) {
+            return car3();
+        } else if (randomCar == 2) {
+            return car2();
+        } else {
+            return car1();
+        }
+    }
+
+    /**
+     * countdown represents a 3-second countdown
+     */
+
+    public void countdown() {
+        try {
+            for (int i = 3; i > 0; i--) {
+                TimeUnit.SECONDS.sleep(1);
+                System.out.println(i);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * finished is the condition met when the game is over
+     */
+
+    public boolean finished() {
+        return score == 9;
+    }
+
+    /**
+     * youSmart is to be called when answering a question correctly
+     */
+
+    public void youSmart() {
+        score++;
+    }
+
+    /**
+     * getUpper gets the upper limits of the user based on their input
+     * @param upperLimit changes the private variable upper to upperLimit
+     */
+    public void getUpper(int upperLimit) {
+        upper = upperLimit;
+    }
+
+    /**
+     * getLower gets the lower limits of the user based on their input
+     * @param lowerLimit changes the private variable to lowerLimit
+     */
+
+    public void getLower(int lowerLimit) {
+        lower = lowerLimit;
+    }
+
+    /**
+     * String beginGame are more rules to be presented when the user reaches the point before they start the game
+     * @return a string of what's going on during the race and some more rules
+     */
+    public String beginGame() {
+        return "You are competing against 3 other cars\nWithin a minute, try to answer all the questions correctly\nIt will be math based questions on simple addition, subtraction, and multiplication\nYou're score will be given to you at the end of the race\nGet a question right, your score increases // Answer incorrectly score goes down\n";
+    }
+
+    /**
+     * keeps track of number of incorrect answers
+     * @param subtracted represents a number based the math problem depending on difficulty of problem
+     * @return score - int subtracted
+     */
+
+    public double retreat (int subtracted) {
+        incorrect++;
+        return score - subtracted;
+    }
+
+    /**
+     * String equation will determine an equation for the user to solve
+     * @return a string with 2 numbers and a random operator to be solved
+     */
+
+    public String equation() {
+
+        int operator = (int) (Math.random() * 3 + 1);
+
+        if (operator == 1) {
+            sign = " - ";
+            return "What is " + firstNumber() + sign + secondNumber() + ": ";
+        } else if (operator == 2) {
+            sign = " - ";
+            return "What is " + firstNumber() + sign + secondNumber() + ": ";
+        } else {
+            sign = " x ";
+            return "What is " + firstNumber() + sign + secondNumber() + ": ";
+        }
+
+    }
+
+    /**
+     * firstNumber and secondNumber represents numbers within the upper and lower limits
+     * @return a random number in the range of limits provided by user
+     */
+
+    private int firstNumber() {
+        random1 = (int) (Math.random() * (upper - lower)) + lower;
+        return random1;
+    }
+
+    private int secondNumber() {
+        random2 = (int) (Math.random() * (upper - lower)) + lower;
+        return random2;
+    }
+
+    /**
+     * checks if your answer to the question is correct
+     * @param answer is an integer representing the answer of the user to the equation provided to them
+     * @return if answer matches with the equation given to them
+     */
+
+    public boolean correct(int answer) {
+        if (sign.equals(" - ")) {
+            return answer == random1 - random2;
+        } else if (sign.equals(" x ")) {
+            return answer == random1 * random2;
+        } else return answer == random1 + random2;
+    }
+
+    /**
+     * toString will determine if you survive or not based on your answers to the questions
+     * @return a string where you survive if answered more than half the questions right, if not then a string where you're dead
+     */
+
+    public String toString() {
+        score++;
+        if (incorrect > (score / 2)) {
+            return "\nGood job! You reached the end of the race with " + score + " questions correctly answered\nHowever, since you got 5 or more questions wrong, You were killed for not finishing first.\nYour position: 2\nYour status: dead";
+        }
+        else {
+            return "\nGood job! You reached the end of the race with " + score + " questions correctly answered\nYour position: 1\nYour status: high";
+        }
+    }
 }
